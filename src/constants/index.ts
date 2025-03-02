6c8a9145baad8953e30a97d8c62252edb7bbf0ff
Renren <johnlawrencemartinez05@gmail.com>
2025-03-02 19:05:20 +0800

    Add files via upload

---
+++ src/constants/index.ts
@@ -0,0 +1,32 @@
+import { FaDiscord, FaTwitch, FaTwitter, FaYoutube } from "react-icons/fa";
+
+export const NAV_ITEMS = [
+  { label: "TRAILER", href: "#hero" },
+  { label: "About", href: "#about" },
+  { label: "Nexus", href: "#nexus" },
+  { label: "Story", href: "#story" },
+  { label: "Contact", href: "#contact" },
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://github.com/sanidhyy/game-website",
+} as const;
+
+export const SOCIAL_LINKS = [
+  {
+    href: "#",
+    icon: FaDiscord,
+  },
+  {
+    href: "#",
+    icon: FaTwitter,
+  },
+  {
+    href: "#",
+    icon: FaYoutube,
+  },
+  {
+    href: "#",
+    icon: FaTwitch,
+  },
+] as const;
